0bc04dece290ec76f772f250cbbcb6104163befc
Herhel Horvat <herhel.horvat@teaminternational.com>
2022-08-29 20:46:22 +0300

    update

--- src/components/InfoBox/index.tsx
+++ src/components/InfoBox/index.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 import ASN1 from '../../utils/asn1';
 import styles from './styles.module.css';
 
+const reHex = /[a-zA-Z]+\n/;
+
 interface InfoBoxProps {
   isAddMode: boolean;
 }
@@ -11,17 +13,30 @@ const InfoBox: FC<InfoBoxProps> = ({ isAddMode }) => {
     const file = e.target.files[0];
     const reader = new FileReader();
 
-    reader.onload = async (e: any) => {
+    reader.onloadend = (e: any) => {
       const certData = e.target.result;
-
       const result = ASN1.decode(certData);
+
       if (result.typeName() !== 'SEQUENCE') {
         throw 'Неправильна структура конверта сертифіката (очікується SEQUENCE)';
       }
-      const tbsCertificate = result;
 
-      console.log(certData);
-      console.log(tbsCertificate);
+      console.log('--- 1 ---');
+      for (let i = 0; i < result.sub[0].sub[5].sub.length; i++) {
+        const item0 = result.sub[0].sub[5].sub[i].sub[0].sub[0];
+        const item1 = result.sub[0].sub[5].sub[i].sub[0].sub[1];
+        const code = item0.content().match(reHex)[0].replace(/\n/, '');
+
+        console.log(`${code}: ${item1.content()}`);
+      }
+
+      // console.dir(item0);
+      // console.log(item0.typeName());
+      // console.log(code);
+
+      // console.dir(item1);
+      // console.log(item1.typeName());
+      // console.dir(item1.content());
     };
 
     reader.readAsBinaryString(file);
